d375f22c75ca1da0e7529790b896a84a4d5b7a5f
d0m591 <dominikostrowski808@gmail.com>
2025-12-07 22:21:48 +0000

    Update 2dProjectileMotion.py

--- 2dProjectileMotion.py
+++ 2dProjectileMotion.py
@@ -1,1 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
+class projectile:
+    def __init__(self, velocity, angle):
+        self.velocity = velocity
+        self.angle = math.radians(angle)
+
+        # Converting velocity into components
+        self.xv = self.velocity * math.cos(self.angle)
+        self.yv = self.velocity * math.sin(self.angle)
+
+rounded = 5
+p1 = projectile(20, 15)
+
+print(f"{p1.yv:.{rounded}f}")
