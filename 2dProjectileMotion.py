d200771422190a1defb037421dc985899719d6b1
d0m591 <dominikostrowski808@gmail.com>
2025-12-08 10:08:35 +0000

    Update 2dProjectileMotion.py

--- 2dProjectileMotion.py
+++ 2dProjectileMotion.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+timestep = 0.2
+
 class projectile:
     def __init__(self, velocity, angle):
         self.velocity = velocity
@@ -11,7 +13,22 @@ class projectile:
         self.xv = self.velocity * math.cos(self.angle)
         self.yv = self.velocity * math.sin(self.angle)
 
-rounded = 5
+        #Arrays for storing positions
+        self.xpos = [0]
+        self.ypos = [0]
+
+    def move(self):
+        #updating xPosition
+        newX = self.xpos[-1] + self.xv * timestep
+        self.xpos.append(newX)
+
+        #updating yPosition
+        newY = self.ypos[-1] + self.yv * timestep
+        self.ypos.append(newY)
+
+
 p1 = projectile(20, 15)
 
-print(f"{p1.yv:.{rounded}f}")
+for i in range(5):
+    print(f"This is x {p1.xpos} and this is y {p1.ypos}")
+    p1.move()
