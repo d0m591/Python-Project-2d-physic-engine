903be9efaadb7dc2e35e92c55d1dd93878bff45d
d0m591 <dominikostrowski808@gmail.com>
2025-12-08 12:26:25 +0000

    Update 2dProjectileMotion.py

--- 2dProjectileMotion.py
+++ 2dProjectileMotion.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-
-timestep = 0.2
+gravity = -9.81
+timestep = 0.02
 
 class projectile:
     def __init__(self, velocity, angle):
@@ -13,22 +13,31 @@ class projectile:
         self.xv = self.velocity * math.cos(self.angle)
         self.yv = self.velocity * math.sin(self.angle)
 
-        #Arrays for storing positions
-        self.xpos = [0]
-        self.ypos = [0]
+        #Arrays for storing positions in form [x,y]
+        self.position = [[0,0]]
+        self.newX = 0
+        self.newY = 0
+
 
     def move(self):
-        #updating xPosition
-        newX = self.xpos[-1] + self.xv * timestep
-        self.xpos.append(newX)
+        self.newX = self.position[-1][0] + self.xv * timestep
+        self.newY = self.position[-1][1] + self.yv * timestep
+        self.position.append([self.newX,self.newY])
+        return self.newX,self.newY
 
-        #updating yPosition
-        newY = self.ypos[-1] + self.yv * timestep
-        self.ypos.append(newY)
+    def gravity(self):
+        self.yv = self.yv + gravity * timestep
+
+p1 = projectile(22, 43)
+
+fig, ax = plt.subplots()
 
 
-p1 = projectile(20, 15)
 
-for i in range(5):
-    print(f"This is x {p1.xpos} and this is y {p1.ypos}")
+while p1.newY >= 0:
     p1.move()
+    p1.gravity()
+
+    ax.plot([p1.newX], [p1.newY], 'go--')
+
+plt.show()
