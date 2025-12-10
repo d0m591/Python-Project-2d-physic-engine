6a9eb61dbceda479d4cf6a38f9542da36ccdde54
d0m591 <dominikostrowski808@gmail.com>
2025-12-10 14:41:57 +0000

    Update 2dProjectileMotion.py

--- 2dProjectileMotion.py
+++ 2dProjectileMotion.py
@@ -1,45 +1,59 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from matplotlib.animation import FuncAnimation
+
 gravity = -9.81
 timestep = 0.02
 
-class projectile:
+class Projectile:
     def __init__(self, velocity, angle):
         self.velocity = velocity
         self.angle = math.radians(angle)
 
-        # Converting velocity into components
+        # velocity components
         self.xv = self.velocity * math.cos(self.angle)
         self.yv = self.velocity * math.sin(self.angle)
 
-        #Arrays for storing positions in form [x,y]
-        self.position = [[0,0]]
-        self.newX = 0
-        self.newY = 0
-
+        # position list
+        self.position = [[0, 0]]
 
     def move(self):
-        self.newX = self.position[-1][0] + self.xv * timestep
-        self.newY = self.position[-1][1] + self.yv * timestep
-        self.position.append([self.newX,self.newY])
-        return self.newX,self.newY
+        x = self.position[-1][0] + self.xv * timestep
+        y = self.position[-1][1] + self.yv * timestep
+        self.position.append([x, y])
 
-    def gravity(self):
-        self.yv = self.yv + gravity * timestep
-
-p1 = projectile(22, 43)
+    def apply_gravity(self):
+        self.yv += gravity * timestep
 
 
+p1 = Projectile(22, 43)
 
-
-while p1.newY >= 0:
-    p1.move()
-    p1.gravity()
-    
-    
 fig, ax = plt.subplots()
-Xlist = [pos[0] for pos in p1.position]
-Ylist = [pos[1] for pos in p1.position]
-plt.plot(Xlist, Ylist, '-')
+ax.set_xlim(0, 60)
+ax.set_ylim(0, 30)
+
+line, = ax.plot([], [], 'r-')
+
+def init():
+    line.set_data([], [])
+    return line,
+
+def update(frame):
+    if p1.position[-1][1] < 0:
+        return line
+
+    p1.move()
+    p1.apply_gravity()
+
+    Xlist = [pos[0] for pos in p1.position]
+    Ylist = [pos[1] for pos in p1.position]
+
+    line.set_data(Xlist, Ylist)
+    return line,
+
+
+ani = FuncAnimation(fig, update, frames=1, init_func=init,
+                    blit=True, interval=5, repeat = True)
+
 plt.show()
