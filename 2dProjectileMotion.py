e0848894d23fed1bac342f5a6b0d9e68d5f6d08d
d0m591 <dominikostrowski808@gmail.com>
2025-12-11 13:59:45 +0000

    Update 2dProjectileMotion.py

--- 2dProjectileMotion.py
+++ 2dProjectileMotion.py
@@ -1,3 +1,5 @@
+from functools import total_ordering
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -27,21 +29,42 @@ class Projectile:
         self.yv += gravity * timestep
 
 
-p1 = Projectile(22, 43)
+p1 = Projectile(100, 15)
+
+#Total time
+totalTime = (2 * p1.yv) / -gravity
+totalDistance = (p1.xv * totalTime)
+totalHeight = (p1.yv * totalTime) + 0.5 * (-gravity * totalTime * totalTime)
 
 fig, ax = plt.subplots()
-ax.set_xlim(0, 60)
-ax.set_ylim(0, 30)
+
 
 line, = ax.plot([], [], 'r-')
 
 def init():
     line.set_data([], [])
+
+    ax.set_xlim([0, totalDistance])
+    ax.set_ylim([0, totalHeight])
     return line,
 
-def update(frame):
-    if p1.position[-1][1] < 0:
-        return line
+blit = True
+def update(Frame):
+    # predict next y before moving
+    next_y = p1.position[-1][1] + p1.yv * timestep
+
+    # if next step would fall below ground, freeze at exactly y=0
+    if next_y < 0:
+
+        p1.position.append([p1.position[-1][0] + p1.xv * timestep, 0])
+
+        Xlist = [pos[0] for pos in p1.position]
+        Ylist = [pos[1] for pos in p1.position]
+
+        line.set_data(Xlist, Ylist)
+
+        ani.event_source.stop()
+        return line,
 
     p1.move()
     p1.apply_gravity()
@@ -53,7 +76,11 @@ def update(frame):
     return line,
 
 
-ani = FuncAnimation(fig, update, frames=1, init_func=init,
-                    blit=True, interval=5, repeat = True)
+ani = FuncAnimation(fig,
+                    update,
+                    frames=1,
+                    init_func=init,
+                    blit=False,
+                    interval=1)
 
 plt.show()
