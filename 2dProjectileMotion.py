0f3e46fef0d30559f8e0ee88b4b00e7289890ee8
d0m591 <dominikostrowski808@gmail.com>
2026-03-03 10:30:13 +0000

    Update 2dProjectileMotion.py

--- 2dProjectileMotion.py
+++ 2dProjectileMotion.py
@@ -4,7 +4,7 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.widgets import Slider, Button as wid
 
 gravity = -9.81
-timestep = 0.02
+timestep = 0.01
 
 class Projectile:
     def __init__(self, velocity, angle, gravity):
@@ -15,8 +15,16 @@ class Projectile:
         self.yv = self.velocity * math.sin(self.angle)
         self.position = [[0, 0]]
         self.is_airborne = True
-        self.timetaken = -2 * self.yv / self.gravity  # works when gravity is negative
-        self.t_peak = -self.yv / self.gravity
+
+        self.initialyv = self.yv
+
+        #with no air resistance
+        self.t_peak = -self.initialyv / self.gravity # works when gravity is negative
+        self.timetaken = 2 * self.t_peak
+
+
+
+
 
     def move(self):
         if self.is_airborne:
@@ -32,7 +40,7 @@ class Projectile:
             self.yv += self.gravity * timestep
 
     def discover(self):
-        maxY = (self.yv * self.t_peak + 0.5 * gravity * (self.t_peak ** 2))
+        maxY = (self.initialyv * self.t_peak + 1 / 2 * self.gravity * (self.t_peak ** 2))
         maxX = (self.xv * self.timetaken)
         return maxX * 1.1 , maxY * 1.1       # I multiply each value to give a buffer so it doesnt go off screen
 
@@ -47,12 +55,9 @@ fig.subplots_adjust(bottom=0.25) #Makes 25% of the bottom of the screen for slid
 ax.set_xlim(0, maxX)
 ax.set_ylim(0, maxY)
 
-
 ax.set_title('Projectile Motion')
 ax.set_xlabel('Distance [m]')
 ax.set_ylabel('Height [m]')
-
-
 
 
 line, = ax.plot([], [], lw=2)
@@ -65,11 +70,10 @@ def update(frame):
     Xlist = [pos[0] for pos in p1.position]
     Ylist = [pos[1] for pos in p1.position]
     line.set_data(Xlist, Ylist)
-    p1.move()
     p1.apply_gravity()
-
-
+    p1.move()
     return line,
+
 #text
 props = dict(boxstyle='round', facecolor='wheat', alpha =0.5)
 textstr = (f"t_flight = {p1.timetaken:.2f}"
@@ -90,7 +94,7 @@ s_velocity = Slider(ax_velocity, "Velocity", 0, 50, valinit=25)
 s_angle = Slider(ax_angle, "Angle", 0, 90, valinit=45)
 
 
-ani = FuncAnimation(fig, update, init_func=init,blit=True, interval=5, repeat=True, frames=1)
+ani = FuncAnimation(fig, update, init_func=init, blit=True, interval=20)
 
 
 plt.show()
